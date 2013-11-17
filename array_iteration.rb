1504801824b2a81b6ec0badac16476c6c877c128
steve <stevedanko1@gmail.com>
2013-11-17 12:37:43 -0500

    Added minimum and maximum

--- array_iteration.rb
+++ array_iteration.rb
@@ -1,7 +1,14 @@
-test_scores = [75, 100, 85, 65, 84, 87, 95]
+
+test_scores=[75, 100, 85, 65, 84, 87, 95, 99]
+
 
 total = test_scores.inject() {|total, score| total += score}
 dataset = test_scores.length.to_f
-puts dataset
-puts total
-puts (total / dataset)
+average = (total / dataset)
+min = test_scores.min
+max = test_scores.max
+
+puts "Given the data set (75, 100, 85, 65, 84, 87, 95, 99)"
+puts "The average test score is: #{"%.2f"%average}"
+puts "The highest test score is: #{max}"
+puts "The lowest test scrore is: #{min}"
